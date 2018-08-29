510a547e2a9dad0ea6c97da81f56f5229e817e07
b51 <b51live@gmail.com>
2018-08-29 11:39:47 +0800

    Fix for auto download

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -6,11 +6,18 @@ log="$localDir/bin/log.log"
 findResult=$(find $localDir -regex $filenameRegex)
 if [ ! -n "$findResult" ]; then
     baseUrl="cn.bing.com"
-    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*hprichbg\(.*\)",id.*')
+    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*hprichbg\(.*\)",d:')
     filename=$(expr "$imgurl" : '.*/\(.*\)')
     localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
     curl -o $localpath $baseUrl/az/hprichbg/$imgurl
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$localpath\""
+#    curl -o $localpath $baseUrl/az/hprichbg/rb/TulipsEquinox_ZH-CN11213785857_1920x1080.jpg
+    echo "$baseUrl/az/hprichbg/$imgurl"
+#    echo "$baseUrl/az/hprichbg/rb/AmalfiCathedral_ZH-CN9007250446.jpg"
+    osascript -e "                              \
+        tell application \"System Events\" to   \
+            tell every desktop to               \
+                set picture to \"$localpath\""
+    osascript -e "display notification \"$filename Downloaded\" with title \"BingWallpaper\""
     echo "$(date +"%Y-%m-%d %H:%M:%S") Downloaded $filename" >> $log
 else
     echo "$(date +"%Y-%m-%d %H:%M:%S") Exist" >> $log
