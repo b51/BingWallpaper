c0da183835faa0b5458bbd536b48e03af0983020
b51 <b51live@gmail.com>
2019-01-21 13:51:52 +0800

    Fix expr rules for new baseurl

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -6,16 +6,15 @@ log="$localDir/bin/log.log"
 findResult=$(find $localDir -regex $filenameRegex)
 if [ ! -n "$findResult" ]; then
     baseUrl="cn.bing.com"
-    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*hprichbg\(.*\)",d:')
+    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : ".*hprichbg\(.*\)', hash")
     filename=$(expr "$imgurl" : '.*/\(.*\)')
     localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
     curl -o $localpath $baseUrl/az/hprichbg/$imgurl
 #    curl -o $localpath $baseUrl/az/hprichbg/rb/TulipsEquinox_ZH-CN11213785857_1920x1080.jpg
     echo "$baseUrl/az/hprichbg/$imgurl"
-#    echo "$baseUrl/az/hprichbg/rb/AmalfiCathedral_ZH-CN9007250446.jpg"
     osascript -e "                              \
         tell application \"System Events\" to   \
-            tell every desktop to               \
+            tell desktop 1 to                   \
                 set picture to \"$localpath\""
     osascript -e "display notification \"$filename Downloaded\" with title \"BingWallpaper\""
     echo "$(date +"%Y-%m-%d %H:%M:%S") Downloaded $filename" >> $log
