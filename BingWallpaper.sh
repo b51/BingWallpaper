46282eb626099c2ea99ddf59a4e9f3d01a6dd3b3
b51 <b51live@gmail.com>
2019-03-12 12:53:13 +0800

    Fix for bing url

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -3,16 +3,19 @@ localDir="/Users/$USER/Pictures/BingWallpaper"
 filenameRegex=".*"$(date "+%Y-%m-%d")".*jpg"
 #filenameRegex=$(date)".*txt"
 log="$localDir/log.log"
+# today: 0, yesterday: 1, tomorrow: -1
+date=0
+bingDailyUrl="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=$date&n=1&mkt=zh-CN"
 
 findResult=$(find $localDir -regex $filenameRegex)
 if [ ! -n "$findResult" ]; then
     baseUrl="cn.bing.com"
-    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*url: "/az/hprichbg\(.*\)"};g_img')
-    #imgurl="\/rb\/OceanDrive_ZH-CN8199064696_1920x1080.jpg"
-    filename=$(expr "$imgurl" : '.*/\(.*\)')
+    #imgurl=$(expr "$(curl -L $bingDailyUrl | grep hprichbg)" : '.*url: "/az/hprichbg\(.*\)"};g_img')
+    imgurl=$(expr "$(curl -L $bingDailyUrl)" : ".*<url>\(.*\)</url>")
+    filename=$(expr "$imgurl" : ".*id=OHR.\(.*\)&amp;rf")
     localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
-    curl -o $localpath $baseUrl/az/hprichbg/$imgurl
-    echo "$baseUrl/az/hprichbg/$imgurl"
+    curl -o $localpath $baseUrl$imgurl
+    echo "$baseUrl$imgurl"
     osascript -e "                              \
         tell application \"System Events\" to   \
             tell desktop 1 to                   \
